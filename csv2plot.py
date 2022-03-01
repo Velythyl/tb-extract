dd006a76faa2f5f623f3f42c03baab6fc34fb49f
Charlie Gauthier <charlie.gauthier@umontreal.ca>
2022-03-01 16:54:26 -0500

    meh

--- csv2plot.py
+++ csv2plot.py
@@ -5,11 +5,13 @@ import pandas as pd
 import seaborn as sns
 import matplotlib
 from matplotlib import pyplot as plt
+import re
 
 from utils import run
 
-smoothings = [0, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95]
+SMOOTHINGS = [0, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95]
 DEBUG = False
+OUTDIR = "img_out"
 
 def do_csv(path, csv_paths, num_runs):
     csv_dict = {}
@@ -26,7 +28,7 @@ def do_csv(path, csv_paths, num_runs):
             x_values = frame['step'].unique()
 
             x_y_e = []
-            for smoothing in smoothings:
+            for smoothing_id, smoothing in enumerate(SMOOTHINGS):
                 acc = []
                 for run_id in range(num_runs):
                     run = frame.iloc[run_id::3,:]
@@ -40,24 +42,12 @@ def do_csv(path, csv_paths, num_runs):
                 y_values = acc.mean(axis=0)
                 y_errors = acc.std(axis=0)
 
-                if DEBUG:
-                    ax = sns.lineplot(x_values, y_values)
-
-                    x_labels = x_values[::2]
-                    plt.locator_params(axis='y', nbins=10)
-                    #ax.set_yticks()
-                    ax.set_xticks(x_labels, x_labels, ha='right', rotation=45, rotation_mode="anchor")
-                    #ax.set_xticklabels(list(map(str, x_values)))
-                    ax.fill_between(x_values, y_values - y_errors, y_values + y_errors, alpha=0.5)
-                    plt.xlabel("Timestep")
-
-                    plot_name, y_axis_name = name.replace("Eval_", "").replace("MultiGrid-", "").replace("-Minigrid", "").replace("-v0", "").split("/")
-
-
-                    plt.title(plot_name)
-                    plt.ylabel(y_axis_name)
-                    plt.tight_layout()
-                    plt.grid(alpha=0.3)
+                if False:# DEBUG:
+                    if "solve" not in csv:
+                        continue
+                    fig, axes = init_plot(True)
+                    just_one_plot(axes=axes, data=(x_values, y_values, y_errors, name), exp_name=path, subplot_y=0, subplot_x=0)
+                    do_finish_plot(fig=fig, axes=axes, csv_name=csv, exp_name=path, smoothing_id=smoothing)
                     plt.show()
                     exit()
 
@@ -73,59 +63,98 @@ def do_csv(path, csv_paths, num_runs):
 
     return csv_dict
 
-def just_one_plot(x_values, y_values, y_errors, name):
+def init_plot(solo):
+    if solo:
+        fig, axes = plt.subplots(1, 1, figsize=(6, 6), sharey=True, squeeze=False)
+    else:
+        fig, axes = plt.subplots(2, 4, figsize=(6*2, 6*4), sharey=True, squeeze=False)
+    return fig, axes
+
+def do_finish_plot(fig, axes, csv_name, exp_name, smoothing_id):
+    plt.xlabel("Timestep")
+
+    plot_name, y_axis_name = csv_name.replace("Eval_", "").replace("MultiGrid-", "").replace("-Minigrid",
+                                                                                             "").replace("-v0",
+                                                                                                         "").replace("/out.csv", "").split(
+        "/")
+
+    plt.title(plot_name)
+
+    y_axis_name = {
+        "solved_rate": "Solve Rate",
+        "median": "Median",
+        "min": "Min"
+    }.get(y_axis_name, y_axis_name)
+
+    plt.ylabel(y_axis_name)
+    plt.tight_layout()
+    plt.grid(alpha=0.3)
+
+    exp_name = re.search(r"\[(.*)\]", exp_name)
+    if exp_name is None:
+        exp_name = "none"
+    else:
+        exp_name = exp_name.group(1)
+        if len(exp_name.split(",")) == 3:
+            ax = plt.gca()
+            ax.get_legend().remove()
+    exp_name = f"{OUTDIR}/random_picks_{exp_name}".replace(",", "_and_")
+
+    os.makedirs(exp_name, exist_ok=True)
+
+    plt.savefig(f"{exp_name}/{plot_name}_{y_axis_name}_{SMOOTHINGS[smoothing_id]}.png", dpi=fig.dpi)
+
+def just_one_plot(axes, data, exp_name, subplot_x, subplot_y):
+    x_values, y_values, y_errors, csv_name = data
+
+    ax = axes[subplot_x, subplot_y]
+
+    line_label = ""
+    if "cooperative" in exp_name:
+        line_label = "cooperative"
+    if "adversarial" in exp_name:
+        line_label = "adversarial"
+
+    plot_name, y_axis_name = csv_name.replace("Eval_", "").replace("MultiGrid-", "").replace("-Minigrid",
+                                                                                             "").replace("-v0",
+                                                                                                         "").replace("/out.csv", "").split(
+        "/")
+
+    ax.title.set_text(plot_name)
+
+    fake_ax = sns.lineplot(axes=ax, x=x_values, y=y_values, label=line_label)
+#    plt.close(fake_ax.fig)
+
+    x_ticks = x_values[1::2]
+    x_labels = list(map(lambda x: f"{str(x / 1000000)[:3]}M", x_ticks))
+
+    #plt.locator_params(axis='y', nbins=10)
+
+    y_ticks = np.arange(11) / 10
+    ax.set_yticks(y_ticks, y_ticks)
+    ax.set_xticks(x_ticks, x_labels, ha='right', rotation=45, rotation_mode="anchor")
+    ax.set_ylim([0, 1.1])
+    # ax.set_xticklabels(list(map(str, x_values)))
+    ax.fill_between(x_values, y_values - y_errors, y_values + y_errors, alpha=0.5)
 
 
 def plot(data, keys, names):
-    for key in keys:
+    # solo plots
 
+    for key in keys:
         if "Eval" and "solve" not in key:
             continue
-
-        for smoothing_id in range(len(smoothings)):
-            fig = plt.figure(figsize=(6,6))
-            for dict_id, dict in enumerate(data):
-                packing = dict[key]
-                x_values, y_values, y_errors, name = packing[smoothing_id]
-
-                line_label = ""
-                if "cooperative" in names[dict_id]:
-                    line_label = "cooperative"
-                if "adversarial" in names[dict_id]:
-                    line_label = "adversarial"
-
-                ax = sns.lineplot(x_values, y_values, label=line_label)
-
-                x_ticks = x_values[::2]
-                x_labels = map(lambda x: f"{str(1000000/x)[:3]}M", x_ticks)
-                plt.locator_params(axis='y', nbins=10)
-
-                y_ticks = np.arange(10)/10
-                ax.set_yticks(y_ticks, y_ticks)
-                ax.set_xticks(x_ticks, x_ticks, ha='right', rotation=45, rotation_mode="anchor")
-                # ax.set_xticklabels(list(map(str, x_values)))
-                ax.fill_between(x_values, y_values - y_errors, y_values + y_errors, alpha=0.5)
-            plt.xlabel("Timestep")
-
-            plot_name, y_axis_name = name.replace("Eval_", "").replace("MultiGrid-", "").replace("-Minigrid",
-                                                                                                 "").replace("-v0",
-                                                                                                             "").split(
-                "/")
-
-            plt.title(plot_name)
-
-            y_axis_name = {
-                "solved_rate": "Solve Rate",
-                "median": "Median"
-            }[y_axis_name]
-
-            plt.ylabel(y_axis_name)
-            plt.tight_layout()
-            plt.grid(alpha=0.3)
-
-            plt.savefig(f"{names[0]}/{plot_name}_{smoothings[smoothing_id]}.png", dpi=fig.dpi)
+        try:
+            for smoothing_id in range(len(SMOOTHINGS)):
+                fig, axes = plt.subplots(1,1)
+                for dict_id, dict in enumerate(data):
+                    packing = dict[key]
+                    just_one_plot(data=packing[smoothing_id], exp_name=names[dict_id], axes=axes, subplot_y=0, subplot_x=0)
+                do_finish_plot(fig=fig, csv_name=key, exp_name=names[0], smoothing_id=smoothing_id, axes=axes)
             #plt.show()
             #exit()
+        except Exception as e:
+            pass
 
     #plt.show()
 
@@ -181,34 +210,67 @@ def do_mp(pair, csv_paths):
     print(pair)
 
     result = [do_csv(pair[0], csv_paths, 3)]
+    keys1 = set(result[0].keys())
     if len(pair) == 2:
         result.append(do_csv(pair[1], csv_paths, 3))
         keys2 = set(result[1].keys())
+        uh = keys1 == keys2  # if we dont do this, sets are counted as empty... fuck python
+        common_keys = keys1.intersection(keys2)
     else:
-        keys2 = set()
-
-    keys1 = set(result[0].keys())
-    uh = keys1 == keys2  # if we dont do this, sets are counted as empty... fuck python
-
-    common_keys = keys1.intersection(keys2)
+        common_keys = keys1
 
     plot(result, common_keys, pair)
+
+    return result, common_keys, pair
 
 
 if __name__ == "__main__":
     path = "/home/velythyl/Desktop/tb-extract/out"
 
+    sns.set()
     sns.set_palette("colorblind")
     matplotlib.rcParams.update({'font.size': 14})
     csv_paths = find_all_experiment_paths(path)
     exp_pairs = find_all_experiment_pairs(path)
 
+    if DEBUG:
+        do_mp(exp_pairs[0], csv_paths)
+        do_csv(exp_pairs[0][0], csv_paths, 3)
 
     import multiprocessing as mp
     pool = mp.Pool()
-    result = pool.starmap(do_mp, zip(exp_pairs, [csv_paths] * len(exp_pairs)))
+    results = pool.starmap(do_mp, zip(exp_pairs, [csv_paths] * len(exp_pairs)))
     pool.close()
     pool.join()
 
+    temp_common_keys = []
+    for (_, common_keys, _) in results:
+        temp_common_keys.append(common_keys)
+    all_common_keys = temp_common_keys[0]
+    for temp_common_key in temp_common_keys:
+        all_common_keys = all_common_keys.intersection(temp_common_key)
 
+    indices = np.argsort(np.array(list(map(lambda xy: xy[0], exp_pairs))))
+
+    for smoothing_id in range(len(SMOOTHINGS)):
+        for key in all_common_keys:
+            #if "Eval" and "solve" not in key:
+            #    continue
+            fig, axes = init_plot(False)
+            for index in indices:
+                for dict_id, dict in enumerate(results[index]):
+                    packing = dict[dict_id][key]
+                    just_one_plot(axes=axes, data=packing[smoothing_id], exp_name=exp_pairs[index][dict_id], subplot_x=index % 2, subplot_y=index % 4) # todo
+            do_finish_plot(fig=fig, csv_name=key, exp_name=exp_pairs[index][0], smoothing_id=smoothing_id)
+            #plt.show()
+            #exit()
+
+"""
+    key2results_dict = {}
+    for key in all_common_keys:
+        for (_, dicts, pairs) in results:
+            if key not in key2results_dict:
+                key2results_dict = []
+            key2results_dict[tuple(pairs)] =
+"""
    # do_csv("/home/velythyl/Desktop/tb-extract/out/none_none_MinigridSettable-v0_cooperative_maxnature_maxlearner_randompicks[agent]/Eval_MultiGrid-SixteenRooms-v0/solved_rate/out.csv", 3)
--- flatten_names.py
+++ flatten_names.py
@@ -7,11 +7,13 @@ import os
 import sys
 
 dr = sys.argv[1]
+filename = sys.argv[2]
+append2 = sys.argv[3]
 
 for root, dirs, files in os.walk(dr):
     for file in files:
-        if file == "Maze_0.95.png":
+        if file == filename:
             filetype = file.split(".")[-1]
 
             spl = root.split("/"); newname = spl[-1]; sup = ("/").join(spl[:-1])
-            shutil.move(root+"/"+file, sup+"/"+newname+f".{filetype}"); shutil.rmtree(root)
+            shutil.move(root+"/"+file, sup+"/"+newname+f"_{append2}.{filetype}"); shutil.rmtree(root)
